25f36f650b3e02f0b5df36200032bb7e9b3845ac
Simon Gate <smgt@buno.se>
2011-04-28 09:21:56 +0200

    Add new git completion

--- git/aliases.bash
+++ git/aliases.bash
@@ -1,3 +1,5 @@
+
+
 # The rest of my fun git aliases
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
@@ -9,4 +11,5 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 
+. /usr/local/etc/bash_completion.d/git-completion.bash
 complete -o default -o nospace -F _git_checkout gco
