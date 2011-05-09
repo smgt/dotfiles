ce861064543f5c42d82d6fc831f89edb82a37102
Simon Gate <smgt@buno.se>
2011-05-09 10:04:51 +0200

    Add GRC config, nice colors on ls etc

--- system/aliases.bash
+++ system/aliases.bash
@@ -1,3 +1,14 @@
+# grc overides for ls
+#   Made possible through contributions from generous benefactors like
+#   `brew install coreutils`
+if $(gls &>/dev/null)
+then
+  alias ls="gls -F --color"
+  alias l="gls -lAh --color"
+  alias ll="gls -l --color"
+  alias la='gls -A --color'
+fi
+
 alias flushdns="dscacheutil -flushcache"
 alias taskpaper="vim ~/Dropbox/todo.taskpaper"
 alias task="taskpaper"
