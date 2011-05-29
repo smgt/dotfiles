d5c4e56e56e71a50130d0c72ec3296bd529d7a05
Simon Gate <smgt@buno.se>
2011-05-29 16:29:40 +0200

    Add git add and difftool aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,8 +1,10 @@
 # The rest of my fun git aliases
 #alias gl='git pull --prune'
+alias ga='git add'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
+alias gdt='git difftool'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
