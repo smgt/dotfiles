7339264bbcd563c82cd12166483cd5146ab0faba
Simon Gate <simon@smgt.me>
2012-09-18 07:23:42 +0200

    Update git commands

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,7 +1,7 @@
 # The rest of my fun git aliases
 #alias gl='git pull --prune'
 alias ga='git add'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gdt='git difftool'
