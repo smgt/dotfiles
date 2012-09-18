b388ba3b247c3e19ed482dddaea50194c17f7865
Simon Gate <simon@smgt.me>
2012-09-18 07:23:09 +0200

    Update zsh config

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -21,24 +21,25 @@ setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 #setopt CORRECT # correct spelling
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
 
+# Share history between sessions
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
 zle -N newtab
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
+bindkey '^[OD' backward-word # Ctrl+arrow left
+bindkey '^[OC' forward-word # Ctrl + arrow right
+bindkey '^[^[[D' beginning-of-line # ESC + arrow left
+bindkey '^[^[[C' end-of-line # ESC + arrow right
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
