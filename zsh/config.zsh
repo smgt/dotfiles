072aa54c6d8fede746666324860b43985db49cdf
Simon Gate <simon@smgt.me>
2013-10-02 21:06:41 +0200

    Update zsh prompt, use promptinit and vcs in zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,11 +1,11 @@
-if [[ -n $SSH_CONNECTION ]]; then
-  export PS1='%m:%3~$(git_info_for_prompt)%# '
-else
-  export PS1='%3~$(git_info_for_prompt)%# '
-fi
+#if [[ -n $SSH_CONNECTION ]]; then
+#  export PS1='%m:%3~$(git_info_for_prompt)%# '
+#else
+#  export PS1='%3~$(git_info_for_prompt)%# '
+#fi
 
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export CLICOLOR=true
+#export LSCOLORS="exfxcxdxbxegedabagacad"
+#export CLICOLOR=true
 
 fpath=($DOTFILES/zsh/functions $fpath)
 
