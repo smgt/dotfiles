1d9cb4befdfc7bd066aac4a2809f04300a4dcab1
Simon Gate <simon@kampgate.se>
2017-11-01 12:44:16 +0100

    [node] nvm

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -2,3 +2,8 @@ if [ -f '/usr/local/opt/nvm/nvm.sh' ];then
   export NVM_DIR="$HOME/.nvm"
   . "/usr/local/opt/nvm/nvm.sh"
 fi
+
+if [ -f '/usr/share/nvm/init-nvm.sh' ];then
+  export NVM_DIR="$HOME/.nvm"
+  . "/usr/share/nvm/init-nvm.sh"
+fi
