71ae99d1dd73813ef8ad7ebbe7cf75de8d492334
Simon Gate <simon@smgt.me>
2018-03-28 10:59:23 +0200

    Git add verbose git diff to commits

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -40,8 +40,8 @@ alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(y
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gdt='git difftool'
-alias gc='git commit'
-alias gca='git commit -a'
+alias gc='git commit -v'
+alias gca='git commit -a -v'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
