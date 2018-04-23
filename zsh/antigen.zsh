7bc07438bcbd1bec17641264a97595f5272eccb6
Simon Gate <simon@smgt.me>
2018-04-23 14:53:53 +0200

    Fix error where antigen dir already exists

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -1,31 +1,31 @@
-if [ ! -d $HOME/.antigen ];then
+if [ ! -f $HOME/.antigen/antigen.zsh ];then
   git clone https://github.com/zsh-users/antigen.git $HOME/.antigen
 fi
 
-#source $DOTFILES/zsh/antigen/antigen.zsh
-source $HOME/.antigen/antigen.zsh
+if [ -f $HOME/.antigen/antigen.zsh ];then
+  source $HOME/.antigen/antigen.zsh
+  # Antigen
+  # Bundles from oh-my-zsh
+  antigen use oh-my-zsh
+  antigen bundle aws
+  antigen bundle git
+  antigen bundle git-extras
+  antigen bundle extract
+  antigen bundle bundler
+  antigen bundle docker
+  antigen bundle docker-compose
+  antigen bundle tmux
+  antigen bundle vagrant
+  antigen bundle node
+  antigen bundle nvm
+  antigen bundle command-not-found
+  antigen bundle gem
+  antigen bundle pip
 
-# Antigen
-# Bundles from oh-my-zsh
-antigen use oh-my-zsh
-antigen bundle aws
-antigen bundle git
-antigen bundle git-extras
-antigen bundle extract
-antigen bundle bundler
-antigen bundle docker
-antigen bundle docker-compose
-antigen bundle tmux
-antigen bundle vagrant
-antigen bundle node
-antigen bundle nvm
-antigen bundle command-not-found
-antigen bundle gem
-antigen bundle pip
+  # other plugins
+  antigen bundle zsh-users/zsh-syntax-highlighting
+  antigen bundle chrissicool/zsh-256color
+  #Tarrasch/zsh-bd
 
-# other plugins
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle chrissicool/zsh-256color
-#Tarrasch/zsh-bd
-
-antigen apply
+  antigen apply
+fi
