63babe64f11995324695ee61b19c876b1a85e8bb
Simon Gate <simon@smgt.me>
2018-11-21 08:11:52 +0100

    Improve polybar startup script

--- xorg/polybar/launch.sh
+++ xorg/polybar/launch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-killall polybar
-
-polybar example &
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+export MONITOR=$(xrandr |grep primary | awk '{print $1}')
+polybar example -q -r &
