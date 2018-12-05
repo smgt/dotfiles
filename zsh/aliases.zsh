ac466a3f3ac3b4d3c673233bca148c76bf888216
Simon Gate <simon@smgt.me>
2018-12-05 10:21:15 +0100

    Show size in human readable when ls

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,12 +8,12 @@ if $(gls &>/dev/null)
 then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
-  alias ll="gls -l --color"
+  alias ll="gls -lh --color"
   alias la='gls -A --color'
 else
   alias ls="ls -F --color"
   alias l="ls -a --color"
-  alias ll="ls -l --color"
+  alias ll="ls -lh --color"
   alias la="ls -A --color"
 fi
 
