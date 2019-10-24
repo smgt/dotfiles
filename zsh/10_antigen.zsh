1da0ea73670ba418873288c1ec947398bdd90b90
Simon Gate <simon@kampgate.se>
2019-10-24 21:05:54 +0200

    [zsh] Remove unsused zsh files

--- zsh/10_antigen.zsh
+++ zsh/10_antigen.zsh
@@ -7,21 +7,14 @@ if [ -f $HOME/.antigen/antigen.zsh ];then
   # Antigen
   # Bundles from oh-my-zsh
   antigen use oh-my-zsh
-  #antigen bundle aws
+  antigen bundle aws
   antigen bundle git
-  antigen bundle git-extras
   antigen bundle extract
   antigen bundle bundler
   antigen bundle docker
   antigen bundle docker-compose
   antigen bundle tmux
-  antigen bundle vagrant
-  antigen bundle node
-  antigen bundle nvm
   antigen bundle command-not-found
-  antigen bundle gem
-  antigen bundle pip
-  antigen bundle ssh-agent
 
   # other plugins
   antigen bundle zsh-users/zsh-syntax-highlighting
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,7 @@
 alias dotvim='vim $DOTFILES'
 alias dotcd='cd $DOTFILES'
 alias reload!='. ~/.zshrc'
+
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
@@ -17,7 +18,6 @@ else
   alias la="ls -A --color"
 fi
 
-alias flushdns="dscacheutil -flushcache"
 alias grep="grep --color"
 
 # Basic directory operations
@@ -32,22 +32,7 @@ alias _='sudo'
 
 ## Mutt in english
 alias mutt="LANG=en_US mutt"
-# The rest of my fun git aliases
-#alias gl='git pull --prune'
-alias ga='git add'
-alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
-alias gdt='git difftool'
-alias gc='git commit -v'
-alias gca='git commit -a -v'
-alias gco='git checkout'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gw='git wtf'
-#   # syntax is way
-#
-alias kv='kitchen verify "$*" && terminal-notifier -title "Kitchen CI" -message "Tests OK" || terminal-notifier -title "Kitchen CI" -message "Tests FAILED"'
+
 alias tmux="TERM=screen-256color-bce tmux"
 
 if (( $+command[mvim] ));then
