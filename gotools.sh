063a4eb81a80bac665f0a2fd760cd99b6c3d7d39
Simon Gate <simon@smgt.me>
2020-02-29 16:39:20 +0100

    [go] Update tools

--- gotools.sh
+++ gotools.sh
@@ -1,7 +1,15 @@
-go get -u github.com/go-delve/delve/cmd/dlv
-curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s -- -b /home/simon/codez/go/bin v2.2.0
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /home/simon/codez/go/bin v1.23.1
-curl -L https://git.io/misspell | bash
-go get github.com/jgautheron/goconst/cmd/goconst
-go get github.com/gordonklaus/ineffassign
-go get -u golang.org/x/tools/...
+#!/bin/sh
+
+
+if [ -z "$GOPATH" ];then
+  echo "Go path is unset."
+  exit 1
+else
+  curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s -- -b "$GOPATH"/bin v2.2.0
+  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$GOPATH"/bin v1.23.1
+  curl -L https://git.io/misspell | sh -s -- -b "$GOPATH"/bin
+  go get -u github.com/jgautheron/goconst/cmd/goconst
+  go get -u github.com/gordonklaus/ineffassign
+  go get -u golang.org/x/tools/...
+  go get -u github.com/go-delve/delve/cmd/dlv
+fi
