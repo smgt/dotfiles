dc7630b1b2f83f39a7839319307fa2e70ac7a628
Simon Gate <simon@smgt.me>
2020-10-25 06:28:20 +0100

    [polybar] fix bug in startup script

--- xorg/polybar/launch.sh
+++ xorg/polybar/launch.sh
@@ -2,7 +2,7 @@
 
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-if [ -z MONITOR ];then
+if [ -z "$MONITOR" ];then
   export MONITOR=$(xrandr |grep primary | awk '{print $1}')
 fi
 polybar default -q -r &
