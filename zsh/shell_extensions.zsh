d22a458ed75581643ba93143f1ea0020c84fdfd8
Simon Gate <simon.gate@readly.com>
2021-03-11 11:02:29 +0100

    [zsh] Remove rbenv and rvm loading

--- zsh/shell_extensions.zsh
+++ zsh/shell_extensions.zsh
@@ -1,11 +1,7 @@
 [[ -f "$HOME/.asdf/asdf.sh" ]] && source "$HOME/.asdf/asdf.sh"
 
 command -v direnv >/dev/null 2>&1 && eval "$(direnv hook zsh)"
-command -v rbenv > /dev/null 2>&1 && eval "$(rbenv init -)"
 if command -v fzf > /dev/null 2>&1; then
   #source /usr/share/fzf/key-bindings.zsh
   #source /usr/share/fzf/completion.zsh
 fi
-
-[[ -d "$HOME/.rvm" ]] && export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
