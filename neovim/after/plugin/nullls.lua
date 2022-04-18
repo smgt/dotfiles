b4107361ccca9cb18caf8d9732b1537c3f4ea274
Simon Gate <simon@kampgate.se>
2022-04-18 08:58:59 +0200

    [nvim] add golangci-lint to null-ls

--- neovim/after/plugin/nullls.lua
+++ neovim/after/plugin/nullls.lua
@@ -4,15 +4,16 @@ if (not status) then return end
 -- local helpers = require("null-ls.helpers")
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.completion.spell,
-        null_ls.builtins.diagnostics.shellcheck,
-        null_ls.builtins.diagnostics.hadolint,
-        null_ls.builtins.diagnostics.jsonlint,
-        null_ls.builtins.formatting.terraform_fmt,
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.protolint,
-        null_ls.builtins.diagnostics.teal,
-        null_ls.builtins.diagnostics.vint,
-    },
+  sources = {
+    null_ls.builtins.completion.spell,
+    null_ls.builtins.diagnostics.shellcheck,
+    null_ls.builtins.diagnostics.hadolint,
+    null_ls.builtins.diagnostics.jsonlint,
+    null_ls.builtins.formatting.terraform_fmt,
+    null_ls.builtins.formatting.goimports,
+    null_ls.builtins.formatting.protolint,
+    null_ls.builtins.diagnostics.teal,
+    null_ls.builtins.diagnostics.vint,
+    null_ls.builtins.diagnostics.golangci_lint
+  },
 })
