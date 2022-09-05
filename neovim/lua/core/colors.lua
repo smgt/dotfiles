b80f88e3ff2fb52a6b62062906810883f4e46e83
Simon Gate <simon@kampgate.se>
2022-09-05 16:24:19 +0200

    [nvim] add vertical split color to tokyo night theme

--- neovim/lua/core/colors.lua
+++ neovim/lua/core/colors.lua
@@ -1,13 +1,11 @@
 -- vim.cmd 'colorscheme material'
 -- vim.g.material_style = "oceanic"
 
--- Example config in Lua
+-- Tokyo night
 vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-
--- Change the "hint" color to the "orange" color, and make the "error" color bright red
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer", "nvimtree" }
+vim.g.tokyonight_colors = { border = "blue" }
 
 -- Load the colorscheme
 vim.cmd [[colorscheme tokyonight]]
