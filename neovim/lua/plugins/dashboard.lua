f311497cd72e23abb747bd02386645a4d27c10c9
Simon Gate <simon@kampgate.se>
2022-09-05 11:49:04 +0200

    [nvim] update dashboard action icons

--- neovim/lua/plugins/dashboard.lua
+++ neovim/lua/plugins/dashboard.lua
@@ -10,17 +10,21 @@ local home = os.getenv('HOME')
 local db = require('dashboard')
 db.custom_header = dashboard_custom_header
 db.custom_center = {
-  { icon = ' ',
+  { icon = ' ',
     desc = 'Find File            ',
     action = 'Telescope find_files',
     shortcut = ',p'
+  },
+  { icon = ' ',
+    desc = 'New File              ',
+    action = 'DashboardNewFile'
   },
   { icon = ' ',
     desc = 'File Browser           ',
     action = 'NvimTreeOpen',
   },
   {
-    icon = ' ',
+    icon = ' ',
     desc = 'Find word            ',
     action = 'Telescope live_grep',
     shortcut = ',l'
