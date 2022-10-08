a982d68b712be4d6ae70f1177c602529d248428f
Simon Gate <simon@kampgate.se>
2022-10-08 22:59:01 +0200

    [bin] add script to delete wine file extensions
    We don't want wine to open anything... 😏

---
+++ bin/wine-rm-open.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+rm -f ~/.local/share/applications/wine-extension*.desktop
+
+rm -f ~/.local/share/icons/hicolor/*/*/application-x-wine-extension*
+
+rm -f ~/.local/share/icons/hicolor/*/*/application-x-wine-extension*
+
+rm -f ~/.local/share/mime/packages/x-wine*
+
+rm -f ~/.local/share/mime/application/x-wine-extension*
+
+update-desktop-database ~/.local/share/applications
+update-mime-database ~/.local/share/mime/
