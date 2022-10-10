bcfd41d0acb8f74a1b976074d0ca71457f94c8b1
Simon Gate <simon@kampgate.se>
2022-10-10 09:01:00 +0000

    [nvim] use vim.keymap.set instead of custom function

--- neovim/lua/core/keymaps.lua
+++ neovim/lua/core/keymaps.lua
@@ -1,77 +1,71 @@
-local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
+local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = ','
 
 -- General
 --
-map('i', '<Up>', '<NOP>')
-map('i', '<Down>', '<NOP>')
-map('i', '<Left>', '<NOP>')
-map('i', '<Right>', '<NOP>')
-map('', '<Up>', '<NOP>')
-map('', '<Down>', '<NOP>')
-map('', '<Left>', '<NOP>')
-map('', '<Right>', '<NOP>')
+vim.keymap.set('i', '<Up>', '<NOP>', opts)
+vim.keymap.set('i', '<Down>', '<NOP>', opts)
+vim.keymap.set('i', '<Left>', '<NOP>', opts)
+vim.keymap.set('i', '<Right>', '<NOP>', opts)
+vim.keymap.set('', '<Up>', '<NOP>', opts)
+vim.keymap.set('', '<Down>', '<NOP>', opts)
+vim.keymap.set('', '<Left>', '<NOP>', opts)
+vim.keymap.set('', '<Right>', '<NOP>', opts)
 
--- map('n', '<C-p>', ':tabnext<CR>')
--- map('n', '<C-o>', ':tabprev<CR>')
-map('n', '<C-p>', ':BufferNext<CR>')
-map('n', '<C-o>', ':BufferPrevious<CR>')
+-- vim.keymap.set('n', '<C-p>', ':tabnext<CR>', opts)
+-- vim.keymap.set('n', '<C-o>', ':tabprev<CR>', opts)
+vim.keymap.set('n', '<C-p>', ':BufferNext<CR>', opts)
+vim.keymap.set('n', '<C-o>', ':BufferPrevious<CR>', opts)
 
 -- TMUX
 --
-map('n', '<C-h>', ':TmuxNavigateLeft<CR>')
-map('n', '<C-j>', ':TmuxNavigateDown<CR>')
-map('n', '<C-k>', ':TmuxNavigateUp<CR>')
-map('n', '<C-l>', ':TmuxNavigateRight<CR>')
-map('n', '<C-\\>', ':TmuxNavigatePrevious<CR>')
+vim.keymap.set('n', '<C-h>', ':TmuxNavigateLeft<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':TmuxNavigateDown<CR>', opts)
+vim.keymap.set('n', '<C-k>', ':TmuxNavigateUp<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':TmuxNavigateRight<CR>', opts)
+vim.keymap.set('n', '<C-\\>', ':TmuxNavigatePrevious<CR>', opts)
 
 -- DAP
 --
-map('n', '<F5>', '<cmd>lua require"dap".continue()<CR>')
-map('n', '<F6>', '<cmd>lua require"dap".step_over()<CR>')
-map('n', '<F7', '<cmd>lua require"dap".step_into()<CR>')
-map('n', '<F8>', '<cmd>lua require"dap".step_out()<CR>')
-map('n', '<F9>', '<cmd>lua require"dap".terminate()<CR>')
-map('n', '<space>b', '<cmd>lua require"dap".toggle_breakpoint()<CR>')
--- map('n', '<F5>', '<cmd>lua require"dap".continue()<CR>')
+vim.keymap.set('n', '<F5>', '<cmd>lua require"dap".continue(, opts)<CR>')
+vim.keymap.set('n', '<F6>', '<cmd>lua require"dap".step_over(, opts)<CR>')
+vim.keymap.set('n', '<F7', '<cmd>lua require"dap".step_into(, opts)<CR>')
+vim.keymap.set('n', '<F8>', '<cmd>lua require"dap".step_out(, opts)<CR>')
+vim.keymap.set('n', '<F9>', '<cmd>lua require"dap".terminate(, opts)<CR>')
+vim.keymap.set('n', '<space>b', '<cmd>lua require"dap".toggle_breakpoint(, opts)<CR>')
+-- vim.keymap.set('n', '<F5>', '<cmd>lua require"dap".continue(, opts)<CR>')
 
 -- Telescope
 --
-map('n', '<Leader>p', '<cmd>Telescope find_files<CR>')
-map('n', '<Leader>l', '<cmd>Telescope live_grep<CR>')
-map('n', '<Leader>b', '<cmd>Telescope buffers<CR>')
-map('n', ';;', '<cmd>Telescope help_tags<CR>')
+vim.keymap.set('n', '<Leader>p', '<cmd>Telescope find_files<CR>', opts)
+vim.keymap.set('n', '<Leader>l', '<cmd>Telescope live_grep<CR>', opts)
+vim.keymap.set('n', '<Leader>b', '<cmd>Telescope buffers<CR>', opts)
+vim.keymap.set('n', ';;', '<cmd>Telescope help_tags<CR>', opts)
 
 -- Barbar
 --
 -- Move to previous/next
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>')
-map('n', '<A-.>', '<Cmd>BufferNext<CR>')
+vim.keymap.set('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+vim.keymap.set('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
 -- Re-order to previous/next
-map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>')
-map('n', '<A->>', '<Cmd>BufferMoveNext<CR>')
+vim.keymap.set('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+vim.keymap.set('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
 -- Goto buffer in position...
-map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>')
-map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>')
-map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>')
-map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>')
-map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>')
-map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>')
-map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>')
-map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>')
-map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>')
-map('n', '<A-0>', '<Cmd>BufferLast<CR>')
+vim.keymap.set('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+vim.keymap.set('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+vim.keymap.set('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+vim.keymap.set('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+vim.keymap.set('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+vim.keymap.set('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+vim.keymap.set('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+vim.keymap.set('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+vim.keymap.set('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+vim.keymap.set('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
 -- Pin/unpin buffer
-map('n', '<A-p>', '<Cmd>BufferPin<CR>')
+vim.keymap.set('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
 -- Close buffer
-map('n', '<A-c>', '<Cmd>BufferClose<CR>')
+vim.keymap.set('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
 -- Wipeout buffer
 --                 :BufferWipeout
 -- Close commands
@@ -81,14 +75,22 @@ map('n', '<A-c>', '<Cmd>BufferClose<CR>')
 --                 :BufferCloseBuffersLeft
 --                 :BufferCloseBuffersRight
 -- Magic buffer-picking mode
--- map('n', '<C-p>', '<Cmd>BufferPick<CR>')
+-- vim.keymap.set('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
 -- Sort automatically by...
-map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>')
-map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>')
-map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>')
-map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>')
+vim.keymap.set('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+vim.keymap.set('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+vim.keymap.set('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+vim.keymap.set('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
 
-map('n', '<leader>im', "<cmd>lua require'telescope'.extensions.goimpl.goimpl{}<CR>]]")
+vim.keymap.set('n', '<leader>im', "<cmd>lua require'telescope'.extensions.goimpl.goimpl{}<CR>]]", opts)
+
+-- Window plugin
+vim.keymap.set('n', '<C-w>z', '<cmd>WindowsMaximize<CR>', opts)
+vim.keymap.set('n', '<C-w>_', '<cmd>WindowsMaximizeVertically<CR>', opts)
+vim.keymap.set('n', '<C-w>|', '<cmd>WindowsMaximizeHorizontally<CR>', opts)
+vim.keymap.set('n', '<C-w>=', '<cmd>WindowsEqualize<CR>', opts)
+
+
 
 -- Other:
 -- :BarbarEnable - enables barbar (enabled by default)
