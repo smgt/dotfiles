e13f16c5fc46b5b5b0dcee1e092717db09f1b622
Simon Gate <simon@kampgate.se>
2022-12-31 14:48:36 +0100

    [nvim] start git commits in Insert mode

--- neovim/lua/core/autocmds.lua
+++ neovim/lua/core/autocmds.lua
@@ -6,22 +6,32 @@ local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 --   command = 'if line("\'\"") > 0 && line("\'\"") <= line("$")| exe "normal g\'\"" | endif',
 -- })
 
-autocmd('BufRead', {
-  pattern = '/tmp/mutt-*',
-  command = 'set tw=72',
+autocmd("BufRead", {
+	pattern = "/tmp/mutt-*",
+	command = "set tw=72",
 })
 
-autocmd('BufRead,BufNewFile', {
-  pattern = '{Guardfile, vagrantfile, Vagrantfile, Gemfile, Rakefile, Thorfile, config.ru}',
-  command = 'set ft=ruby',
+autocmd("BufRead,BufNewFile", {
+	pattern = "{Guardfile, vagrantfile, Vagrantfile, Gemfile, Rakefile, Thorfile, config.ru}",
+	command = "set ft=ruby",
 })
 
-autocmd('FileType', {
-  pattern = 'make',
-  command = 'set noexpandtab',
+autocmd("FileType", {
+	pattern = "make",
+	command = "set noexpandtab",
 })
 
-autocmd('BufRead,BufNewFile', {
-  pattern = '*.cr',
-  command = 'set ft=crystal',
+autocmd("BufRead,BufNewFile", {
+	pattern = "*.cr",
+	command = "set ft=crystal",
+})
+
+autocmd({ "BufRead", "BufNewFile" }, { pattern = { "*.txt", "*.md", "gitcommit" }, command = "setlocal spell" })
+
+-- start git commit messages in insert mode
+augroup("bufcheck", { clear = true })
+autocmd("FileType", {
+	group = "bufcheck",
+	pattern = { "gitcommit", "gitrebase" },
+	command = "startinsert | 1",
 })
