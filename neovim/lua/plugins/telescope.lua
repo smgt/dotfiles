a552ff567acef639d3777752a3bfd50ff3bf89d1
Simon Gate <simon@kampgate.se>
2023-01-31 14:26:49 +0100

    [nvim] Remove gt key mapping in lsp config

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -4,22 +4,22 @@ if not status then
 end
 
 local actions = require("telescope.actions")
-local tconfig = require("telescope.config")
-local vimgrep_arguments = { unpack(tconfig.values.vimgrep_arguments) }
-local trouble = require("trouble.providers.telescope")
+-- local tconfig = require("telescope.config")
+-- local vimgrep_arguments = { unpack(tconfig.values.vimgrep_arguments) }
+-- local trouble = require("trouble.providers.telescope")
 
 -- I want to search in hidden/dot files.
-table.insert(vimgrep_arguments, "--hidden")
-table.insert(vimgrep_arguments, "--no-ignore")
+-- table.insert(vimgrep_arguments, "--hidden")
+-- table.insert(vimgrep_arguments, "--no-ignore")
 -- I don't want to search in the `.git` directory.
-table.insert(vimgrep_arguments, "--glob")
-table.insert(vimgrep_arguments, "!.git/*")
+-- table.insert(vimgrep_arguments, "--glob")
+-- table.insert(vimgrep_arguments, "!.git/*")
 
 telescope.setup({
 	defaults = {
 		layout_strategy = "vertical",
 		layout_config = { height = 0.95 },
-		vimgrep_arguments = vimgrep_arguments,
+		-- vimgrep_arguments = vimgrep_arguments,
 		mappings = {
 			i = {
 				["<esc>"] = actions.close,
