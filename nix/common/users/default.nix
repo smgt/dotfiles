127763b4f1265a6a62e0bee8a8e0e5ef56c8cfee
Simon Gate <simon@kampgate.se>
2024-06-20 16:36:46 +0200

    [nix] small fix

--- nix/common/users/default.nix
+++ nix/common/users/default.nix
@@ -4,7 +4,7 @@
   users.users.simon = {
     isNormalUser = true;
     initialPassword = "pw123";
-    extraGroups = [ "wheel" "docker" "audio" "plugdb" "libvirtd" "dialout" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "docker" "audio" "plugdb" "libvirtd" "dialout" ]; # Enable `sudo` for the user.
     shell = pkgs.zsh;
 
     openssh.authorizedKeys.keys = [
@@ -12,7 +12,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICvWEz/NePRXK1BTGU6zbr3ASlQTtqFwT8sEvWgj09ly simon@kale"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPYMEc/iU8oLrAse2Z3h5Xq7eZPSalLByghFtE5ETwnI simon@paprika"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE4Oka+Y67A+5hCIKX3ZAuWra407WWQKocd5Djl/AD5x simon@sugarsnap"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEWR9PLHWl96IzTyEcql1WE1sd3K2gEBVaIAWBCm71E9 elvan"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEWR9PLHWl96IzTyEcql1WE1sd3K2gEBVaIAWBCm71E9 fjotton"
     ];
   };
 
