d9528da694fdfef755d0d4dd06a9630eccd73a1e
Simon Gate <simon@kampgate.se>
2024-06-22 12:03:04 +0200

    [nix] add fennel configuration

---
+++ nix/machines/fennel/configuration.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ../../common
+      ../../common/users/home-manager.nix
+      /etc/nixos/hardware-configuration.nix
+      /etc/nixos/networking.nix
+      ../../modules/tailscale.nix
+    ];
+
+  boot.tmp.cleanOnBoot = true;
+  zramSwap.enable = true;
+
+  environment.variables.HOSTNAME = "fennel";
+
+  services.openssh.enable = true;
+
+  networking = {
+    hostName = "fennel"; # Define your hostname.
+    networkmanager.enable = true; # Easiest to use and most distros use this by default.
+    firewall.enable = false;
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  };
+
+  # Select internationalisation properties.
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  system.stateVersion = "23.11";
+}
