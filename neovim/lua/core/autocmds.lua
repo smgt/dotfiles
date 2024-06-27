2fa7285a55f97815e62a479d2e02b7424a0b2bde
Simon Gate <simon@kampgate.se>
2024-06-27 07:45:21 +0200

    [nvim] Disable inlay hints

--- neovim/lua/core/autocmds.lua
+++ neovim/lua/core/autocmds.lua
@@ -36,15 +36,15 @@ autocmd("FileType", {
   command = "startinsert | 1",
 })
 
-autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client.server_capabilities.inlayHintProvider then
-      vim.lsp.inlay_hint.enable(true)
-    end
-  end,
-})
+-- autocmd("LspAttach", {
+--   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+--   callback = function(args)
+--     local client = vim.lsp.get_client_by_id(args.data.client_id)
+--     if client.server_capabilities.inlayHintProvider then
+--       vim.lsp.inlay_hint.enable(true)
+--     end
+--   end,
+-- })
 
 -- autocmd("BufWritePost", {
 --   pattern = "*",
