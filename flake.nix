1cff22eed765efb356163a785a36e02b9af6a04a
Simon Gate <simon@kampgate.se>
2024-09-18 14:57:16 +0200

    Add flake for dotfiles

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            stylua
+            lua-language-server
+          ];
+        };
+      });
+    };
+}
