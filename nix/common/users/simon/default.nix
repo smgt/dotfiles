22fe42ab34be39b6080fc85ce4c062a572306a88
Simon Gate <simon@kampgate.se>
2024-10-14 10:30:09 +0200

    [nix] longer warn timeout in direnv when using flakes

--- nix/common/users/simon/default.nix
+++ nix/common/users/simon/default.nix
@@ -51,6 +51,11 @@ in {
     enable = true;
     nix-direnv.enable = true;
     enableZshIntegration = true;
+    config = {
+      global = {
+        warn_timeout = "5m";
+      };
+    };
   };
 
   programs.zoxide = {
