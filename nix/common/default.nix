3ebcfc663aa05696d8d143cfe3ff1bf6b70fc7ce
Simon Gate <simon@kampgate.se>
2024-11-02 10:21:18 +0100

    [nix] update gc

--- nix/common/default.nix
+++ nix/common/default.nix
@@ -40,7 +40,16 @@ in with lib; {
       "simon"
     ];
 
-    nix.gc.automatic = true;
+    nix.optimise = {
+      automatic = true;
+      dates = ["01:32"];
+    };
+
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
 
     time.timeZone = "Europe/Stockholm";
 
