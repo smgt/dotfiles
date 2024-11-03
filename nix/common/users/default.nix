4d9536a86da5acb2f7afa86bfeee14d017b9792c
Simon Gate <simon@kampgate.se>
2024-11-03 17:34:54 +0100

    [nix] simon passwordless sudo

--- nix/common/users/default.nix
+++ nix/common/users/default.nix
@@ -1,25 +1,37 @@
 { config, pkgs, ... }:
 
 {
-  users.users.simon = {
-    isNormalUser = true;
-    initialPassword = "pw123";
-    extraGroups = [ "wheel" "docker" "audio" "plugdb" "libvirtd" "dialout" ]; # Enable `sudo` for the user.
-    shell = pkgs.zsh;
+  users = {
+    users.simon = {
+      isNormalUser = true;
+      initialPassword = "pw123";
+      extraGroups = [ "wheel" "docker" "audio" "plugdb" "libvirtd" "dialout" ]; # Enable `sudo` for the user.
+      shell = pkgs.zsh;
 
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG+sxYLFUyDMdOUSax4E5zsshbWtVhOOxJnMeIezhaw/ simon@elli"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICvWEz/NePRXK1BTGU6zbr3ASlQTtqFwT8sEvWgj09ly simon@kale"
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINn48B6XiiLpbupjUtmz2Puh3FXhAEBW4PqmqCXhBJv+AAAABHNzaDo= simon@kale"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPYMEc/iU8oLrAse2Z3h5Xq7eZPSalLByghFtE5ETwnI simon@paprika"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE4Oka+Y67A+5hCIKX3ZAuWra407WWQKocd5Djl/AD5x simon@sugarsnap"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEWR9PLHWl96IzTyEcql1WE1sd3K2gEBVaIAWBCm71E9 fjotton"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJTcbcxjbrvVWrnUlJSxCNT/O2Zg7HOR6Ne7RsP2PEhf tick"
-    ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG+sxYLFUyDMdOUSax4E5zsshbWtVhOOxJnMeIezhaw/ simon@elli"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICvWEz/NePRXK1BTGU6zbr3ASlQTtqFwT8sEvWgj09ly simon@kale"
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINn48B6XiiLpbupjUtmz2Puh3FXhAEBW4PqmqCXhBJv+AAAABHNzaDo= simon@kale"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPYMEc/iU8oLrAse2Z3h5Xq7eZPSalLByghFtE5ETwnI simon@paprika"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE4Oka+Y67A+5hCIKX3ZAuWra407WWQKocd5Djl/AD5x simon@sugarsnap"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEWR9PLHWl96IzTyEcql1WE1sd3K2gEBVaIAWBCm71E9 fjotton"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJTcbcxjbrvVWrnUlJSxCNT/O2Zg7HOR6Ne7RsP2PEhf tick"
+      ];
+    };
+
+    users.root = {
+      shell = pkgs.zsh;
+      openssh.authorizedKeys.keys = config.users.users.simon.openssh.authorizedKeys.keys;
+    };
   };
 
-  users.users.root.openssh.authorizedKeys.keys =
-    config.users.users.simon.openssh.authorizedKeys.keys;
-  users.users.root.shell = pkgs.zsh;
+  security.sudo.extraRules = [
+    {
+      users = [ "simon" ];
+      commands = [
+        { command = "ALL"; options = [ "NOPASSWD" ]; }
+      ];
+    }
+  ];
 }
 
