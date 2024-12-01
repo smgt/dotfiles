cbebce3a8a0bad37a1483ac0d88e3f994af82a19
Simon Gate <simon@kampgate.se>
2024-12-01 19:02:54 +0100

    [nix] fix zsh prompt and disable LOCAL_OPTIONS to get ctrl-z back

--- nix/common/users/simon/default.nix
+++ nix/common/users/simon/default.nix
@@ -232,12 +232,17 @@ in {
       };
 
       initExtra = ''
-        source $DOTFILES/zsh/prompt.zsh
+        # Load custom prompts
+        fpath=($DOTFILES/zsh/prompts $fpath)
+        autoload -Uz promptinit; promptinit
+        prompt smgt
+
+        #source $DOTFILES/zsh/prompt.zsh
 
         setopt NO_BG_NICE # don't nice background tasks
         setopt NO_HUP
         setopt NO_LIST_BEEP
-        setopt LOCAL_OPTIONS # allow functions to have local options
+        #setopt LOCAL_OPTIONS # allow functions to have local options
         setopt LOCAL_TRAPS # allow functions to have local traps
         setopt PROMPT_SUBST
         setopt COMPLETE_IN_WORD
