ff57ecab9d910c31717a2c3ffaa61c117e6c56de
Simon Gate <simon@kampgate.se>
2025-06-03 14:10:48 +0200

    [nix] add darkman setup

--- nix/common/users/simon/default.nix
+++ nix/common/users/simon/default.nix
@@ -67,13 +67,47 @@ in {
   };
 
   xdg.enable = true;
-  services = { ssh-agent.enable = true; };
+  services = {
+    ssh-agent.enable = true;
+    darkman = {
+      enable = true;
+      settings = {
+        dbusserver = true;
+        usegeoclue = false;
+      };
+      lightModeScripts = {
+        notify = ''
+          notify-send --app-name="darkman" --urgency=low --icon=weather-clear "light mode"
+        '';
+        gnome = ''
+          gsettings set org.gnome.desktop.interface gtk-theme Adwaita
+          gsettings set org.gnome.desktop.interface color-scheme prefer-light
+        '';
+        xsettings = ''
+          exec xfconf-query -c xsettings -p /Net/ThemeName -s 'Adawaita'
+        '';
+      };
+      darkModeScripts = {
+        notify = ''
+          notify-send --app-name="darkman" --urgency=low --icon=weather-clear-night "dark mode"
+        '';
+        gnome = ''
+          gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
+          gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+        '';
+        xsettings = ''
+          exec xfconf-query -c xsettings -p /Net/ThemeName -s 'Adawaita-dark'
+        '';
+      };
+    };
+  };
 
   programs = {
     # xdg.configFile.nvim = {
     #   source = ../../../../neovim;
     #   recursive = true;
     # };
+    #
 
     btop = { enable = true; };
 
