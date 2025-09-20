5e287154c6c94f6e9709f7fc42741189fdf71081
Simon Gate <simon@kampgate.se>
2025-09-20 15:40:13 +0200

    [nix] add yazi to hm

--- nix/home/dev.nix
+++ nix/home/dev.nix
@@ -17,6 +17,22 @@ in
       programs = {
         awscli.enable = true;
 
+        yazi = {
+          enable = true;
+          enableZshIntegration = true;
+          settings = {
+            log = {
+              enabled = false;
+            };
+            manager = {
+              show_hidden = true;
+              sort_by = "mtime";
+              sort_dir_first = true;
+              sort_reverse = true;
+            };
+          };
+        };
+
         murp = {
           enable = true;
           jira.ticketQuery = vars.murp.jira.ticketQuery;
